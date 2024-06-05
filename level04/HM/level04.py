f1ba0b966fe84d486e26c8927ea8aba22524b1cf
Rati <168450897+ratiebitashvil1@users.noreply.github.com>
2024-06-05 15:44:55 +0400

    Add files via upload

---
+++ level04/HM/level04.py
@@ -0,0 +1,115 @@
+
+
+# +
+
+print(20 + 20)
+print(27 + 32)
+print(35 + 64)
+print(12 + 32)
+print(74312 + 2)
+print(421 + 14)
+print(52+45)
+print(17 +43)
+print(6261+17189)
+print(45627+462)
+print(271+4321)
+print(1244+1271)
+print(123+321)
+print(532+214)
+print(124+821125)
+
+
+# -
+
+
+print(27 + 32)
+print(35 + 64)
+print(12 + 32)
+print(74312 + 2)
+print(421 + 14)
+print(52+45)
+print(17 +43)
+print(6261+17189)
+print(45627+462)
+print(271+4321)
+print(1244+1271)
+print(123+321)
+print(532+214)
+print(124+821125)
+
+
+# *
+
+
+print(27 * 32)
+print(35 * 64)
+print(12 * 32)
+print(74312 * 2)
+print(421 * 14)
+print(52*45)
+print(17*43)
+print(6261*17189)
+print(45627*462)
+print(271*4321)
+print(1244*1271)
+print(123*321)
+print(532*214)
+print(124*821125)
+
+
+# //
+
+
+print(27 // 32)
+print(35 // 64)
+print(12 // 32)
+print(74312 // 2)
+print(421 // 14)
+print(52//45)
+print(17//43)
+print(6261//17189)
+print(45627//462)
+print(271//4321)
+print(1244//1271)
+print(123//321)
+print(532//214)
+print(124//821125)
+
+
+# **
+
+
+
+print(27 ** 3)
+print(35 ** 4)
+print(12 ** 2)
+print(74312 ** 2)
+print(421 ** 4)
+print(52**45)
+print(17**43)
+print(6261**4)
+print(45627**2)
+print(271**3)
+print(1244**7)
+print(123**3)
+print(532**4)
+print(124**2)
+
+
+
+name="rati"
+last_name ="ebitashvili"
+age="14"
+hobi="programireba"
+conrty="saqartvelo"
+
+print(name+" "+last_name+" "+age+" "+hobi+" "+conrty)
+
+
+
+
+
+
+
+
+
